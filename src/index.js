d90d37e68d2c1363f80a3c9713b78cc8945248b7
Jason Carpenter <jtcarpenter@gmail.com>
2018-09-02 16:25:38 +0100

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {Provider} from 'react-redux';
+import App from './containers/app/App.jsx';
+import appStore from './store/appStore';
+
+ReactDOM.render(
+    <Provider store={appStore}>
+        <App/>
+    </Provider>,
+    document.getElementById('app')
+);
---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+const path = require('path');
+const PATHS = {
+    app: path.join(__dirname, 'src/'),
+    public: path.join(__dirname, 'public')
+};
+
+module.exports = {
+    entry: {
+        app: PATHS.app + '/index.js'
+    },
+    output: {
+        path: PATHS.public,
+        filename: 'bundle.js'
+    },
+    devtool: 'eval-source-map',
+    devServer: {
+        inline: true,
+        port: 3333,
+        contentBase: PATHS.public
+    },
+    module: {
+        rules: [
+            {
+                test: /\.jsx?$/,
+                include: PATHS.app,
+                loader: 'babel-loader',
+                query: {
+                    presets: ['@babel/env', '@babel/react']
+                }
+            }
+        ]
+    }
+}
